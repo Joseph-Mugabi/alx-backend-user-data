b08a3b6cd9ff1b249f5719b3209a3983a1c0b0e6
Joseph-Mugabi <mugabijoshgreen@gmail.com>
2023-08-02 20:55:45 +0300

    filter logs

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -11,8 +11,9 @@ import os
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
-def filter_datum(fields, redaction, message, separator):
-    """filters the logs"""
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """filters logsthat replaces fields with redaction in message """
     for field in fields:
         message = re.sub(f'({field}=)(.*?){separator}',
                          rf'\1{redaction}{separator}', message)
