0b9ce9aeece79d8b4ba790b9c81719564dcde82c
Joseph-Mugabi <mugabijoshgreen@gmail.com>
2023-08-02 21:10:26 +0300

    log formatting

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -18,3 +18,22 @@ def filter_datum(fields: List[str], redaction: str,
         message = re.sub(f'({field}=)(.*?){separator}',
                          rf'\1{redaction}{separator}', message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """
+    Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        self.fields = fields
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        """format"""
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
