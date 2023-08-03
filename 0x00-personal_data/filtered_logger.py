4f72a719aac892739247f26eec277e77c46b9433
Joseph-Mugabi <mugabijoshgreen@gmail.com>
2023-08-03 14:27:58 +0300

    db

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -59,3 +59,23 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
     db_name = os.environ.get('PERSONAL_DATA_DB_NAME')
     return mysql.connector.connect(user=db_user, password=db_pass,
                                    host=db_host, database=db_name)
+
+
+def main() -> None:
+    """get all users"""
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users;")
+    logger = get_logger()
+    for row in cursor:
+        message = f"name={row[0]}; email={row[1]}; phone={row[2]}; " +\
+            f"ssn={row[3]}; password={row[4]}; ip={row[5]}; " +\
+            f"last_login={row[6]}; user_agent={row[7]};"
+        message = filter_datum(list(PII_FIELDS), '***', message, '; ')
+        logger.INFO(message)
+    cursor.close()
+    db.close()
+
+
+if __name__ == '__main__':
+    main()
